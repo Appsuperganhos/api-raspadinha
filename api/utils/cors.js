043da6cc0950ba72eacb52494cf76515adee64c5
App <jonatatoick081@gmail.com>
2025-08-11 14:07:57 -0300

    Update cors.js

--- api/utils/cors.js
+++ api/utils/cors.js
@@ -1,16 +1,19 @@
 // api/utils/cors.js
 export function applyCors(res, origin = 'https://raspamaster.site') {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', origin); // ou '*'
+  res.setHeader('Access-Control-Allow-Origin', origin); // pode trocar para '*'
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Accept');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, X-Requested-With, Accept'
+  );
 }
 
 export function handlePreflight(req, res, origin = 'https://raspamaster.site') {
   applyCors(res, origin);
   if (req.method === 'OPTIONS') {
     res.status(200).end();
-    return true; // já respondeu o preflight
+    return true; // já respondeu ao preflight
   }
   return false;
 }
